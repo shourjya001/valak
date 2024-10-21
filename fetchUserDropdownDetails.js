a4753a8451b5abbc575af81ddb602370c27ec6ac
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-10-21 11:02:48 +0530

    Update fetchUserDropdownDetails.js

---
+++ fetchUserDropdownDetails.js
@@ -0,0 +1,30 @@
+function fetchUserDropdownDetails(ctype, dtype){
+if(ctype =='codria'){
+dtajson = {
+"sub_group_code": 'fetchLegalEntityBasedonID',
+"searchType": $("#selectsgr_code option: selected").val(),
+"codria": $('input[name="codria"]:checked').val(),
+};
+}
+if(dtype == 'le')
+dtajson = {
+"searchType": 'fetchLimitsBasedonCodria',
+"sub_group_code": $("#selectsgr_code option:selected").val(),
+"le_code": $("#selectle_code option:selected").val(),
+"codria": $('input[name="codria"]:checked').val(),
+}
+},
+$.ajax({
+};
+type: 'POST',
+url: "dbe_cfl_user_accessTransferSave.php",
+data: dtajson,
+dataType: "json",
+success: function(res) {
+$('#loading_wrap' ).show();
+if(res != false)(
+getUserDropdownDetails(res);
+}else{
+console.log(res);
+$('#loading_wrap').fadeOut('slow');
+}});}
