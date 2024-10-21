7f7d4489ba4e3d5cc257e95f0463ffa1939131b6
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-10-21 11:47:45 +0530

    Update commonserch.js

---
+++ commonserch.js
@@ -0,0 +1,46 @@
+function commonsearch (searchType, searchString, searchParam, stype, check) {
+$.ajax({
+type: 'POST',
+url: "dbe_cfl_user_accessTransferSave.php",
+data: ("searchType":searchType, "searchString": searchString ),
+dataType: "json",
+success: function(resultData) {
+if (resultData = false) {
+if(check == 0) populateSearchOptions (stype, resultData);
+if(stype=='sgr') {
+commonsearch('fetchLEBasedonSGR', resultData[0]['id'], searchParam, 'le',0);
+}else if(stype =='le') {
+if($("#selectsgr_code option:selected").val() I='" && $("#selectle_code option:selected").val() != ''){ checkForCreditFiles($("#selectsgr_code option: selected").val(),$("#selectle_code option:selected").val()); }else if($("#txtle_code").val() != '" || $("#txtle_name").val() != ''){
+sgrdata = {};
+sgrdata['sgr'] = ('id': resultData[0].sgr_code, 'name': resultData[0].sgr_name);
+populateSearchOptions('sgr',sgrdata);
+// LE dropdown
+ledata = {};
+ledata['le']=['id': resultData[0].le_code, 'name': resultData[0].le_name); populateSearchOptions('le', ledata);
+checkForCreditFiles($("#selectsgr_code option: selected").val(), $("#selectle_code option:selected").val());
+}else {
+if(check == 0) {
+populateSearchOptions('sgr', resultData);
+commonsearch('fetchLegalEntityBasedonID', resultData[0]['id'], searchParam, 'le', 1);
+}
+}
+}
+$('#loading_wrap').fadeOut('slow');
+}else{
+if(check == 0) {
+alert("No Matching Results Found...! or You are not authorized to see this SGR/LE,please contact the Banking Administrator-"
+}
+$('#loading_wrap').fadeOut('slow');
+if(stype =='le') {
+if(check == 1){
+$("#codria_span").html('');
+alert("No references are available for this LE..1");
+}else{
+emptydropdown (stype)
+}else if(stype =='sgr')
+{
+var rtype='le';
+emptydropdown (stype);
+emptydropdown(rtype);
+}
+}}});
